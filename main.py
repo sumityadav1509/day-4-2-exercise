c38e5e635c0c2a6b8f4b9d8d7881b5fb68433a5b
sumityadav1509 <sumityadav2005@gmail.com>
2020-12-22 20:01:35 +0000

    Completed the treasure map code.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-# dirty_dozen= ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"] 
-fruits=["Strawberries", "Nectarines", "Apples", "Grapes",  "Peaches", "Cherries", "Pears"] 
-vegetables=["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]  
+row1 = ["boo", "boo", "boo"]
+row2 = ["boo", "boo", "boo"]
+row3 = ["boo", "boo", "boo"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the tresure ? ")
 
-dirty=[fruits,vegetables]
+horizontal=int(position[0])
+vertical=int(position[1]) 
 
-print(dirty)
+selected_row=map[horizontal-1]
+selected_row[vertical-1] = "X"
+
+
+
+
+
+
+
+print(f"{row1}\n{row2}\n{row3}")
+ 
